cfb4059e626888226af58304561c063f93d9a4ab
Aditya Kashinath Vibhute <adityakvibhute2003@gmail.com>
2025-12-28 16:13:11 +0530

    Added cors

--- src/main/java/com/example/To_do_App/security/SecurityConfig.java
+++ src/main/java/com/example/To_do_App/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.To_do_App.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,6 +32,7 @@ public class SecurityConfig {
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
